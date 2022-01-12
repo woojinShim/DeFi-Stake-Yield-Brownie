83e52e80a52ed6b728b95a7284afa8e061e4af67
Woojin <cmshim22@gmail.com>
2022-01-13 02:07:57 +0900

    Unit Test2

--- tests/unit/test_token_farm.py
+++ tests/unit/test_token_farm.py
@@ -1,6 +1,7 @@
-from brownie import network
+from brownie import network, exceptions
 from scripts.helpful_scripts import (
     LOCAL_BLOCKCHAIN_ENVIRONMENTS,
+    INITIAL_PRICE_FEED_VALUE,
     get_account,
     get_contract,
 )
@@ -21,5 +22,39 @@ def test_set_price_feed_contract():
         dapp_token.address, price_feed_address, {"from": account}
     )
 
+
+def test_stake_tokens(amount_staked):
+    # Arrange
+    if network.show_active() not in LOCAL_BLOCKCHAIN_ENVIRONMENTS:
+        pytest.skip("Only for local testing!")
+    account = get_account()
+    token_farm, dapp_token = deploy_token_farm_and_dapp_token()
+    # Act
+    dapp_token.approve(token_farm.address, amount_staked, {"from": account})
+    token_farm.stakeTokens(amount_staked, dapp_token.address, {"from": account})
     # Assert
-    assert token_farm.tokenPriceFeedMapping(dapp_token.address) == price_feed_address
+    assert (
+        token_farm.stakingBalance(dapp_token.address, account.address) == amount_staked
+    )
+    assert token_farm.uniqueTokensStaked(account.address) == 1
+    assert token_farm.stakers(0) == account.address
+    return token_farm, dapp_token
+
+
+def test_issue_tokens(amount_staked):
+    # Arrange
+    if network.show_active() not in LOCAL_BLOCKCHAIN_ENVIRONMENTS:
+        pytest.skip("Only for local testing!")
+    account = get_account()
+    token_farm, dapp_token = test_stake_tokens(amount_staked)
+    starting_balance = dapp_token.balanceOf(account.address)
+    # Act
+    token_farm.issueTokens({"from": account})
+    # Arrange
+    # we are staking 1 dapp_token == in price to 1 ETH
+    # so we should get 2,000 dapp tokens in reward
+    # since the price of eth is $2,000
+    assert (
+        dapp_token.balanceOf(account.address)
+        == starting_balance + INITIAL_PRICE_FEED_VALUE
+    )

888f84c3ae1ef4805f9eb05a51ef0c4c35b51668
Woojin <cmshim22@gmail.com>
2022-01-13 15:15:34 +0900

    MetaMask Connect

--- front_end/src/App.tsx
+++ front_end/src/App.tsx
@@ -1,19 +1,24 @@
-import React from 'react';
-import { DAppProvider, ChainId } from '@usedapp/core';
-import {Header} from "./components/Header"
-import {Container} from "@material-ui/core"
+import React from 'react'
+import { ChainId, DAppProvider } from "@usedapp/core"
+import { Header } from "./components/Header"
+import { Container } from "@material-ui/core"
+import { Main } from "./components/Main"
 
 function App() {
   return (
     <DAppProvider config={{
-      supportedChains: [ChainId.Kovan, ChainId.Rinkeby],
+      supportedChains: [ChainId.Kovan],
+      notifications: {
+        expirationPeriod: 1000,
+        checkInterval: 1000
+      }
     }}>
       <Header />
       <Container maxWidth="md">
-              <div>hi</div>
+        <Main />
       </Container>
     </DAppProvider>
-  );
+  )
 }
 
-export default App;
+export default App
